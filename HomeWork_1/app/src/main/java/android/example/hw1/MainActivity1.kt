5f1dcc9b782441da849b33eb38afdc3e14d6a243
buzka16 <buzka16@ya.ru>
2022-02-26 14:39:50 +0300

    Homework 1 done

---
+++ HomeWork_1/app/src/main/java/android/example/hw1/MainActivity1.kt
@@ -0,0 +1,31 @@
+package android.example.hw1
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
+
+class MainActivity1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main1)
+        val secondActivityResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val res = result.data?.extras?.get("result")
+                    if (res is Array<*> && res.isArrayOf<String>())
+                        findViewById<ListView>(R.id.listView).adapter = ArrayAdapter(
+                            this,
+                            R.layout.list_item,
+                            res
+                        )
+                }
+            }
+        findViewById<Button>(R.id.button1).setOnClickListener {
+            secondActivityResult.launch(Intent(this, MainActivity2::class.java))
+        }
+    }
+}
