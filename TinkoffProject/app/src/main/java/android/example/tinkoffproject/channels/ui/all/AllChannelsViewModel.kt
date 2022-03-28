7e40628c4ccb13919d49286173c7f07af43c44c6
buzka16 <buzka16@ya.ru>
2022-03-28 20:52:40 +0300

    HW 5 done

--- TinkoffProject/app/src/main/java/android/example/tinkoffproject/channels/ui/all/AllChannelsViewModel.kt
+++ TinkoffProject/app/src/main/java/android/example/tinkoffproject/channels/ui/all/AllChannelsViewModel.kt
@@ -2,30 +2,55 @@ package android.example.tinkoffproject.channels.ui.all
 
 import android.example.tinkoffproject.channels.model.ChannelItem
 import android.example.tinkoffproject.channels.ui.BaseChannelsViewModel
+import android.example.tinkoffproject.contacts.model.ContactItem
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class AllChannelsViewModel : BaseChannelsViewModel() {
+    override val allChannels = mutableListOf<ChannelItem>()
+
 
     init {
         loadChannels()
+        subscribe()
     }
 
     override fun loadChannels() {
-        val newList = mutableListOf<ChannelItem>()
-        for (i in 0..19) {
-            val channel = ChannelItem("#Channel ${i + 1}")
-            newList.add(channel)
-            if (topics[channel.name] == null)
-                topics[channel.name] = mutableListOf()
-            for (j in 0..5) {
-                topics[channel.name]?.add(
-                    ChannelItem(
-                        "TOPIC ${j + 1}",
-                        true,
-                        parentChannel = channel.name
-                    )
-                )
+        _isAsyncTaskCompleted.value = false
+        Completable.fromCallable {}
+            .subscribeOn(Schedulers.io())
+            .doOnSubscribe {
+                allChannels.apply {
+                    for (i in 0..19) {
+                        val channel = ChannelItem("#Channel ${i + 1}")
+                        this.add(channel)
+                        if (topics[channel.name] == null)
+                            topics[channel.name] = mutableListOf()
+                        for (j in 0..5) {
+                            topics[channel.name]?.add(
+                                ChannelItem(
+                                    "TOPIC ${j + 1}",
+                                    true,
+                                    parentChannel = channel.name
+                                )
+                            )
+                        }
+                    }
+                }
             }
-        }
-        _channels.value = newList
+            .delay(1, TimeUnit.SECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onComplete = {
+                    _channels.value = allChannels
+                    _isAsyncTaskCompleted.value = true
+                }
+            )
+            .addTo(compositeDisposable)
     }
+
 }
